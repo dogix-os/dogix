18601b5576e288e98915f65c33fa578bd7edab82
Neptuneia <spikegem2@gmail.com>
2023-02-04 23:58:09 -0700

    Revert "renamed iso building thingies to dogix instead of arch"
    This reverts commit 4895899fa382d83628cc6d7f9a1b6d83b328a6d4.

--- profiledef.sh
+++ profiledef.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="dogixlinux"
-iso_label="DOGIX_$(date +%Y%m)"
-iso_publisher="Dogix Linux <https://archlinux.org>"
-iso_application="Dogix Linux Live/Rescue CD"
-iso_version="2/23"
-install_dir="dogix"
+iso_name="archlinux"
+iso_label="ARCH_$(date +%Y%m)"
+iso_publisher="Arch Linux <https://archlinux.org>"
+iso_application="Arch Linux Live/Rescue CD"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
            'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
